7004513a9e5756b2c8748af42146fb3c6e11db22
ZaneC04 <zenithzsc@gmail.com>
2025-10-03 10:34:51 -0400

    solved printFibbonacci

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
- 
+    let a = 0
+    let b = 1
+    for (let i = 0; i < n; i++) {
+        console.log(a);
+        let next = a + b;
+        a = b;
+        b = next;
+    }
 }
 
+console.log("example one");
 printFibonacci(5);  // should print 0, 1, 1, 2, 3
+console.log("example two")
 printFibonacci(10); // should print 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
